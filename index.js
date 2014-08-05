1a8de9d038535bbc4887e2fda0e9d9728cdbaa42
Nolan Lawson <nolan.lawson@gmail.com>
2014-08-04 22:29:00 -0400

    use local express-pouchdb instead of pouchdb-server

--- index.js
+++ index.js
@@ -2,46 +2,88 @@
 
 'use strict';
 
-var child = require('child_process');
+var SKIM_REMOTE = 'https://skimdb.npmjs.com/registry';
+var SKIM_LOCAL = 'http://localhost:16984/skimdb';
+var FAT_LOCAL = 'http://localhost:16984/fullfatdb';
+
+var httpProxy = require('http-proxy');
 var Promise = require('bluebird');
-var request = require('request');
+var fs = require('fs');
 var Fullfat = require('npm-fullfat-registry');
+var pouchdbServerLite = require('./pouchdb-server-lite');
+var app = pouchdbServerLite.app;
+var PouchDB = pouchdbServerLite.PouchDB;
 
-child.fork(__dirname + '/node_modules/.bin/pouchdb-server', ['-p', '16984'])
+var skimPouch = new PouchDB('skimdb');
+var fatPouch = new PouchDB('fullfatdb');
 
-Promise.resolve().then(function () {
-  return new Promise(function (resolve, reject) {
-    setTimeout(resolve, 5000);
+// replicate from central skimdb
+var upToDate;
+var startingTimeout = 1000;
+var backoff = 1.1;
+
+function replicateForever() {
+  skimPouch.replicate.from(SKIM_REMOTE, {
+    live: true
+  }).on('change', function (change) {
+    console.log('Replicating skimdb, last_seq is: ' + change.last_seq);
+  }).on('uptodate', function () {
+    console.log('local skimdb is up to date');
+    skimPouch.put({_id: '_local/upToDate', upToDate: true});
+    upToDate = true;
+  }).on('error', function (err) {
+    console.error(err);
+    // just keep going
+    setTimeout(replicateForever, Math.round(startingTimeout * backoff));
   });
+}
+replicateForever();
+
+// start doing exciting shit
+Promise.resolve().then(function () {
+  return skimPouch.get('_local/upToDate').catch(function (err) {
+    if (err.status !== 404) {
+      throw err;
+    }
+    return {}; // default doc
+  });
+}).then(function (doc) {
+  upToDate = doc.upToDate;
+  if (upToDate) {
+    console.log('local skimdb is up to date');
+  }
 }).then(function () {
-  // create the skim and fullfat dbs
-  return Promise.all(['skimdb', 'fullfatdb'].map(function (db) {
-    return Promise.promisify(request.put)('http://127.0.0.1:16984/' + db).catch(function (err) {
-      if (err.status !== 412) { // 412 means it already exists
-        throw err;
-      }
-    });
-  }));
+  // trick Fullfat into never doing automatic syncing; we don't use it for that
+  return Promise.promisify(fs.writeFile)('registry.seq', '99999999999');
 }).then(function () {
-  return Promise.promisify(request.post)('http://127.0.0.1:16984/_replicate', {json: {
-    source: 'https://skimdb.npmjs.com/registry',
-    target: 'skimdb',
-    continuous: true
-  }});
-}).then(function () {
-  var ff = new Fullfat({
-    skim: 'http://localhost:16984/skimdb', 
-    fat: 'http://localhost:16984/fullfatdb',
+  var fullFat = new Fullfat({
+    skim: upToDate ? SKIM_LOCAL : SKIM_REMOTE,
+    fat: FAT_LOCAL,
     seq_file: 'registry.seq',
     missing_log: 'missing.log'
-  }).on('error', function (err) {
+  });
+  fullFat.on('error', function (err) {
     console.error(err);
     throw err;
   });
-  // TODO: ff.getDoc({id: 'pouch'})
-  // seems to be the magic call
+  return fullFat;
+}).then(function (fullFat) {
+  // start user-facing proxy server
+  var proxy = httpProxy.createProxyServer({});
+  var server = require('http').createServer(function (req, res) {
+    proxy.web(req, res, {
+      target: 'http://127.0.0.1:16984'
+    });
+  });
+  server.listen(5080, function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Proxy server started at http://127.0.0.1:5080');
+    }
+  });
 }).catch(function (err) {
   console.error(err);
   throw err;
-})
+});
 
---
+++ pouchdb-server-lite.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var express = require('express');
+var corser  = require('corser');
+var favicon = require('serve-favicon');
+var argv    = require('optimist').argv;
+var port    = +(argv.p || argv.port || 16984);
+var logger  = argv.l || argv.log || 'dev';
+var app     = express();
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(corser.create({
+  methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE'],
+  supportsCredentials: true,
+  requestHeaders: corser.simpleRequestHeaders.concat(["Authorization", "Origin", "Referer"])
+}));
+
+if (logger !== 'off') {
+  app.use(require('morgan')(logger));
+}
+
+var expressPouchDB = require('express-pouchdb');
+var PouchDB = require('pouchdb');
+app.use(expressPouchDB(PouchDB));
+app.listen(port, function () {
+  console.log('\npouchdb-server listening on port ' + port + '.');
+  console.log('\nnavigate to http://localhost:' + port + '/_utils for the Fauxton UI.\n');
+}).on('error', function (e) {
+  if (e.code === 'EADDRINUSE') {
+    console.error('\nError: Port ' + port + ' is already in use.');
+    console.error('Try another one, e.g. pouchdb-server -p ' +
+      (parseInt(port) + 1) + '\n');
+  } else {
+    console.error('Uncaught error: ' + e);
+    console.error(e.stack);
+  }
+});
+
+process.on('SIGINT', function () {
+  process.exit(0);
+});
+
+exports.app = app;
+exports.PouchDB = PouchDB;
