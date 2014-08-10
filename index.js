0b1fb9d8d40fe7f2131d8e66840107f2c5733b9a
Calvin Metcalf <cmetcalf@appgeo.com>
2014-08-10 14:17:49 -0400

    break apart

--- index.js
+++ index.js
@@ -2,134 +2,129 @@
 
 'use strict';
 
-var argv = require('yargs').argv;
-if (argv.h || argv.help) {
-  console.log('\nusage:');
-  console.log('-h, --help        : show help');
-  console.log('-p, --port        : port (default 5080)');
-  console.log('-P, --pouch-port  : pouchdb-server port (default 16984)');
-  console.log('-l, --log         : pouchdb-server log level (dev|short|tiny|combined|off)');
-  console.log('-r, --remote      : remote fullfatdb (default https://registry.npmjs.org)');
-  console.log('-R, --remote-skim : remote skimdb (default https://skimdb.npmjs.com/registry)\n');
-  return process.exit(0);
-}
-
-var FAT_REMOTE = argv.r || argv.remote || 'https://registry.npmjs.org';
-var SKIM_REMOTE = argv.R || argv['remote-skim'] || 'https://skimdb.npmjs.com/registry';
-var port = argv.p || argv.port || 5080;
-var startingTimeout = 1000;
-var logger  = argv.l || argv.log || 'dev';
-console.log('\nWelcome!');
-console.log('To start using local-npm, just run: ');
-console.log('\n  $ npm set registry http://127.0.0.1:' + port);
-console.log('\nTo switch back, you can run: ');
-console.log('\n  $ npm set registry ' + FAT_REMOTE);
-var backoff = 1.1;
-var request = require('request');
-var Promise = require('bluebird');
-var pouchdbServerLite = require('./pouchdb-server-lite');
-var express = require('express');
-var app = express();
-var PouchDB = pouchdbServerLite.PouchDB;
-var skimRemote = new PouchDB(SKIM_REMOTE);
-var skimPouch = new PouchDB('skimdb');
-var base = 'http://localhost:' + port + '/tarballs';
-var level = require('level');
-var db = level('./binarydb');
-var through = require('through2');
-if (logger !== 'off') {
-  app.use(require('morgan')(logger));
-}
-app.use(require('compression')());
-app.get('/:name', function (req, res) {
-  skimPouch.get(req.params.name).then(function (doc) {
-    var docs = changeTarballs(base, doc);
-    res.json(docs);
-  }).catch(function (e) {
-    request.get(FAT_REMOTE + req.url).pipe(res);
-  });
-});
-app.get('/:name/:version', function (req, res) {
-  skimPouch.get(req.params.name).then(function (doc) {
-    res.json(changeTarballs(base, doc).versions[req.params.version]);
-  }).catch(function (e) {
-    request.get(FAT_REMOTE + req.url).pipe(res);
-  });
-});
-app.get('/tarballs/:name/:version.tgz', function (req, res) {
-  var id = req.params.name + '-' + req.params.version;
-  db.get(id, function (err, resp) {
-    if (!err) {
-      res.set('content-type', 'application/octet-stream');
-      res.set('content-length', resp.length);
-      return res.send(resp);
+module.exports = function (FAT_REMOTE, SKIM_REMOTE, port, logger) {
+  FAT_REMOTE = FAT_REMOTE || 'https://registry.npmjs.org';
+  SKIM_REMOTE =  SKIM_REMOTE || 'https://skimdb.npmjs.com/registry';
+  port = port  || 5080;
+  logger =  logger || 'dev';
+  var startingTimeout = 1000;
+  function log(msgs) {
+    if (logger !== 'off') {
+      console.log.apply(console, arguments);
     }
-    var buffs = [];
-    var get = request.get(FAT_REMOTE + '/' + req.params.name + '/-/' + id + '.tgz');
-    get.pipe(res);
-    get.pipe(through(function (chunk, _, next) {
-      buffs.push(chunk);
-      next();
-    }, function (next) {
-      next();
-      var buff = Buffer.concat(buffs);
-      db.put(id, buff, function (err){
-        if (err) {
-          console.log(err);
-        }
+  }
+  log('\nWelcome!');
+  log('To start using local-npm, just run: ');
+  log('\n  $ npm set registry http://127.0.0.1:' + port);
+  log('\nTo switch back, you can run: ');
+  log('\n  $ npm set registry ' + FAT_REMOTE);
+  var backoff = 1.1;
+  var request = require('request');
+  var Promise = require('bluebird');
+  var express = require('express');
+  var app = express();
+  var PouchDB = require('./pouchdb-server-lite').PouchDB;
+  var skimRemote = new PouchDB(SKIM_REMOTE);
+  var skimPouch = new PouchDB('skimdb');
+  var base = 'http://localhost:' + port + '/tarballs';
+  var level = require('level');
+  var db = level('./binarydb');
+  var through = require('through2');
+  if (logger !== 'off') {
+    app.use(require('morgan')(logger));
+  }
+  app.use(require('compression')());
+  app.use(require('serve-favicon')(__dirname + '/favicon.ico'));
+  app.get('/:name', function (req, res) {
+    skimPouch.get(req.params.name).then(function (doc) {
+      var docs = changeTarballs(base, doc);
+      res.json(docs);
+    }).catch(function (e) {
+      request.get(FAT_REMOTE + req.url).pipe(res);
+    });
+  });
+  app.get('/:name/:version', function (req, res) {
+    skimPouch.get(req.params.name).then(function (doc) {
+      res.json(changeTarballs(base, doc).versions[req.params.version]);
+    }).catch(function (e) {
+      request.get(FAT_REMOTE + req.url).pipe(res);
+    });
+  });
+  app.get('/tarballs/:name/:version.tgz', function (req, res) {
+    var id = req.params.name + '-' + req.params.version;
+    db.get(id, function (err, resp) {
+      if (!err) {
+        res.set('content-type', 'application/octet-stream');
+        res.set('content-length', resp.length);
+        return res.send(resp);
+      }
+      var buffs = [];
+      var get = request.get(FAT_REMOTE + '/' + req.params.name + '/-/' + id + '.tgz');
+      get.pipe(res);
+      get.pipe(through(function (chunk, _, next) {
+        buffs.push(chunk);
+        next();
+      }, function (next) {
+        next();
+        var buff = Buffer.concat(buffs);
+        db.put(id, buff, function (err){
+          if (err) {
+            log(err);
+          }
+        });
+      }));
+    });
+  });
+  function changeTarballs(base, doc) {
+    Object.keys(doc.versions).forEach(function (key) {
+      doc.versions[key].dist.tarball = base + '/' + doc.name + '/' + key + '.tgz';
+    });
+    return doc;
+  }
+  app.all('/*', function (req, res) {
+    res.send(500);
+  });
+  var sync;
+  function replicateSkim() {
+    skimRemote.info().then(function (info) {
+      sync = skimPouch.replicate.from(skimRemote, {
+        live: true,
+        batch_size: 2000
+      }).on('change', function (change) {
+        var percent = Math.min(100,
+          (Math.floor(change.last_seq / info.update_seq * 10000) / 100).toFixed(2));
+        log('Replicating skimdb, last_seq is: ' + change.last_seq + ' (' + percent + '%)');
+      }).on('uptodate', function () {
+        log('local skimdb is up to date');
+      }).on('error', function (err) {
+        console.error('error during replication with skimdb');
+        console.error(err);
+        restartReplication();
       });
-    }));
-  });
-});
-function changeTarballs(base, doc) {
-  Object.keys(doc.versions).forEach(function (key) {
-    doc.versions[key].dist.tarball = base + '/' + doc.name + '/' + key + '.tgz';
-  });
-  return doc;
-}
-app.all('/*', function (req, res) {
-  res.send(500);
-});
-var sync;
-function replicateSkim() {
-  skimRemote.info().then(function (info) {
-    sync = skimPouch.replicate.from(skimRemote, {
-      live: true,
-      batch_size: 2000
-    }).on('change', function (change) {
-      var percent = Math.min(100,
-        (Math.floor(change.last_seq / info.update_seq * 10000) / 100).toFixed(2));
-      console.log('Replicating skimdb, last_seq is: ' + change.last_seq + ' (' + percent + '%)');
-    }).on('uptodate', function () {
-      console.log('local skimdb is up to date');
-    }).on('error', function (err) {
-      console.error('error during replication with skimdb');
+    }).catch(function (err) {
+      console.error('error doing info() on ' + SKIM_REMOTE);
       console.error(err);
       restartReplication();
     });
-  }).catch(function (err) {
-    console.error('error doing info() on ' + SKIM_REMOTE);
-    console.error(err);
-    restartReplication();
-  });
-}
-function restartReplication() {
-  // just keep going
-  startingTimeout *= backoff;
-  setTimeout(replicateSkim, Math.round(startingTimeout));
-}
-replicateSkim();
+  }
+  function restartReplication() {
+    // just keep going
+    startingTimeout *= backoff;
+    setTimeout(replicateSkim, Math.round(startingTimeout));
+  }
+  replicateSkim();
 
-app.listen(port);
+  app.listen(port);
 
-process.on('SIGINT', function () {
-  // close gracefully
-  sync.cancel();
-  db.close(function () {
-    skimPouch.close().then(function () {
-      process.exit();
-    }, function () {
-      process.exit();
+  process.on('SIGINT', function () {
+    // close gracefully
+    sync.cancel();
+    db.close(function () {
+      skimPouch.close().then(function () {
+        process.exit();
+      }, function () {
+        process.exit();
+      });
     });
   });
-});
+};
