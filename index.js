fcd496ade3d874374cb0a1a242f7e72fc07abdd1
Calvin Metcalf <cmetcalf@appgeo.com>
2014-08-10 15:21:02 -0400

    simplify

--- index.js
+++ index.js
@@ -36,23 +36,19 @@ module.exports = function (FAT_REMOTE, SKIM_REMOTE, port, logger) {
   app.use(require('compression')());
   app.use(require('serve-favicon')(__dirname + '/favicon.ico'));
   app.get('/:name', function (req, res) {
-    skimPouch.get(req.params.name).then(function (doc) {
+    skimPouch.get(req.params.name).catch(function (e) {
+      return skimRemote.get(req.params.name);
+    }).then(function (doc) {
       var docs = changeTarballs(base, doc);
       res.json(docs);
-    }).catch(function (e) {
-      request.get({
-        url: SKIM_REMOTE + req.url,
-        json: true
-      }, function (e, r, doc) {
-        if (e) {
-          return res.send(500, e);
-        }
-        if (r.statusCode > 399) {
-          return res.send(r.statusCode);
-        }
-        var docs = changeTarballs(base, doc);
-        res.json(docs);
-      });
+    }, function (e) {
+      var status = e.status || e.statusCode;
+      var msg = e.message || e.error;
+      if (status && status > 399) {
+        res.send(status, msg);
+      } else {
+        res.send(500, msg || 'unknown error');
+      }
     });
   });
   app.get('/:name/:version', function (req, res) {
