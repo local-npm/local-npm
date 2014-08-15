dd59206d4489a4be0f769f4a4a55404d2715e914
Calvin Metcalf <cmetcalf@appgeo.com>
2014-08-15 09:34:35 -0400

    add help

--- bin.js
+++ bin.js
@@ -6,15 +6,23 @@ var yargs = require('yargs')
   .alias('h', 'help')
   .default('p', 5080)
   .alias('p', 'port')
+  .describe('p', 'port')
   .default('P', 16984)
   .alias('P', 'pouch-port')
+  .describe('P', 'pouchdb-server port')
   .alias('l', 'log')
+  .describe('l', 'log level')
   .default('r', 'https://registry.npmjs.org')
   .alias('r', 'remote')
+  .describe('r', 'remote fullfatdb')
   .default('R', 'https://skimdb.npmjs.com/registry')
   .alias('R', 'remote-skim')
+  .describe('R', 'remote skimdb')
   .default('u', 'http://127.0.0.1:5080')
-  .alias('u', 'url-base');
+  .alias('u', 'url-base')
+  .describe('u', 'base url you want clients to use for fetching tarballs')
+  .version(require('./package.json').version, 'v')
+  .alias('v', 'version');
 
 var argv = yargs.argv;
 
