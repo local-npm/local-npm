d64a5604516f92eb7f520d0234943a2e8f3dc6bd
Nolan Lawson <nolan.lawson@gmail.com>
2016-01-09 17:49:06 -0500

    keep adding tests

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 # We need to run this as a bash script, because if we try to do the
 # npmrc shenanigans inside of npm or node, then npm will never pick
 # up on the changes.
@@ -10,6 +8,7 @@ npm run write-npmrc
 
 echo "Starting local-npm..."
 if [[ $COVERAGE == 1 ]]; then
+  ./node_modules/.bin/rimraf coverage
   ./node_modules/.bin/istanbul cover ./lib/bin.js -- --directory install_dir --port 3030 --pouch-port 3040 &
 else
   ./lib/bin.js --directory install_dir --port 3030 --pouch-port 3040 &
@@ -17,6 +16,7 @@ fi
 PID=$!
 
 npm run run-test
+MAIN_STATUS=$?
 
 if [[ ! -z $PID ]]; then
   echo "Killing local-npm..."
@@ -30,4 +30,12 @@ wait $PID
 if [[ $COVERAGE == 1 ]]; then
   echo "Checking coverage..."
   ./node_modules/.bin/istanbul check-coverage --lines 100 --function 100 --statements 100 --branches 100
+  COVERAGE_STATUS=$?
+  if [[ $COVERAGE_STATUS != 0 ]]; then
+    exit $COVERAGE_STATUS
+  fi
+fi
+
+if [[ $MAIN_STATUS != 0 ]]; then
+  exit $MAIN_STATUS
 fi
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var Promise = require('bluebird');
+global.Promise = Promise; // forces pouchdb to use bluebird
+Promise.longStackTraces();
 var exec = require('child-process-promise').exec;
 var denodeify = require('denodeify');
 var mkdirp = denodeify(require('mkdirp'));
@@ -7,8 +10,12 @@ var rimraf = denodeify(require('rimraf'));
 var ncp = denodeify(require('ncp').ncp);
 var fs = require('fs');
 var statAsync = denodeify(fs.stat);
-var writeFileAsync = denodeify(fs.writeFile);
 var path = require('path');
+var fetch = require('node-fetch');
+var PouchDB = require('pouchdb');
+var memdown = require('memdown');
+var pkg = require('../package.json');
+
 require('chai').should();
 
 var WORK_DIR = 'work_dir';
@@ -18,7 +25,6 @@ describe('main test suite', function () {
   this.timeout(30000);
 
   beforeEach(async () => {
-    await writeFileAsync('.npmrc', 'registry=http://127.0.0.1:3030/', 'utf-8');
     await rimraf(WORK_DIR);
     await mkdirp(WORK_DIR);
   });
@@ -29,11 +35,127 @@ describe('main test suite', function () {
 
   it('does a simple npm install', async () => {
     await ncp('./test/project1', WORK_DIR);
+
     var registry = await exec('npm config get registry', {cwd: WORK_DIR});
     registry.stdout.trim().should.equal('http://127.0.0.1:3030/');
+
     await exec('npm install', {cwd: WORK_DIR});
-    var stat = await statAsync(path.resolve(WORK_DIR, 'node_modules', 'lodash'));
+
+    var stat = await statAsync(
+      path.resolve(WORK_DIR, 'node_modules', 'lodash'));
     stat.isDirectory().should.equal(true, 'lodash dir should exist');
+  });
+
+  it('does a simple npm install multiple times', async () => {
+    await ncp('./test/project1', WORK_DIR);
+
+    for (var i = 0; i < 5; i++) {
+      if (i === 2) {
+        await exec('npm cache clear', {cwd: WORK_DIR});
+      }
+      await exec('npm install', {cwd: WORK_DIR});
+
+      var stat = await statAsync(
+        path.resolve(WORK_DIR, 'node_modules', 'lodash'));
+      stat.isDirectory().should.equal(true, 'lodash dir should exist');
+    }
+
+  });
+
+  it('does a more advanced npm install', async () => {
+    await ncp('./test/project2', WORK_DIR);
+
+    await exec('npm install', {cwd: WORK_DIR});
+
+
+    var modules = ['lodash', 'colors', 'mkdirp'];
+
+    for (var mod of modules) {
+      var stat = await statAsync(path.resolve(WORK_DIR, 'node_modules', mod));
+      stat.isDirectory().should.equal(true, `${mod} dir should exist`);
+    }
+  });
+
+  it('has a www/ page', async () => {
+    var res = await fetch('http://127.0.0.1:3030/_browse');
+    res.status.should.equal(200);
+    var text = await res.text();
+    text.should.match(/^<!DOCTYPE html>/);
+  });
+
+  it('has a _skimdb/ path', async () => {
+    // this is used by the www/ page
+    var res = await fetch('http://127.0.0.1:3030/_skimdb');
+    res.status.should.equal(200);
+    var json = await res.json();
+    // e.g.: {
+    //   "adapter": "leveldb",
+    //   "auto_compaction": false,
+    //   "backend_adapter": "LevelDOWN",
+    //   "db_name": "skimdb",
+    //   "disk_size": 1511780,
+    //   "doc_count": 1200,
+    //   "instance_start_time": "1452376899205",
+    //   "update_seq": 1200
+    // }
+    json.db_name.should.equal('skimdb');
+    json.doc_count.should.be.a('number');
+  });
+
+  it('has a valid couchdb endpoint at _skimdb/ ', async () => {
+    var db = new PouchDB('http://127.0.0.1:3030/_skimdb');
+    var info = await db.info();
+    info.doc_count.should.be.above(0);
+    var docs = await db.allDocs({include_docs: true, limit: 1});
+    docs.rows.should.have.length(1);
+  });
+
+
+  // TODO: this fails
+  it.skip('can replicate from _skimdb/ ', async () => {
+    var db = new PouchDB('temp', {db: memdown});
+    await db.replicate.from('http://127.0.0.1:3030/_skimdb');
+    var info = await db.info();
+    info.doc_count.should.be.above(0);
+    var docs = await db.allDocs({include_docs: true, limit: 1});
+    docs.rows.should.have.length(1);
+    await db.destroy();
+  });
+
+  it('has a version at /', async () => {
+    var res = await fetch('http://127.0.0.1:3030');
+    var json = await res.json();
+    json.version.should.equal(pkg.version);
+  });
+
+  it('can do `npm info`', async () => {
+    var res = (await exec('npm info pouchdb')).stdout;
+    res.should.be.a('string');
+    res.should.match(/pouchdb/);
+  });
+
+  it('installs packages from local cache', async () => {
+
+    // fetch packages from local cache using pouchdb
+    var db = new PouchDB('http://127.0.0.1:3030/_skimdb');
+    var docs = await db.changes({limit: 10});
+
+    // there's a weird old package called "Babel" that doesn't
+    // npm install correctly
+    var packages = docs.results
+      .map(row => row.id)
+      .filter(id => ['Babel'].indexOf(id) == -1);
+
+    await ncp('./test/project3', WORK_DIR);
+
+    // check that they are npm installed
+    for (var packageName of packages) {
+      await exec(`npm install ${packageName}`, {cwd: WORK_DIR});
+      var stat = await statAsync(
+        path.resolve(WORK_DIR, 'node_modules', packageName));
+      stat.isDirectory().should.equal(true,
+        `package ${packageName} should exist`);
+    }
   });
 
 });
