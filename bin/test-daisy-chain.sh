8a44932efaec0bb45e3700166ef6bdc297438262
Nolan Lawson <nolan.lawson@gmail.com>
2016-01-10 18:33:15 -0500

    improve coverage, selectively disable istanbul

--- bin/test-daisy-chain.sh
+++ bin/test-daisy-chain.sh
@@ -7,12 +7,12 @@ echo "Starting local-npm..."
 
 # this is the "source" local-npm
 ./lib/bin.js --directory install_dir2 --port 3031 \
-  --pouch-port 3041 --log debug &
+  --pouch-port 3041 --log error &
 PID1=$!
 
 # and this is the "client" local-npm
 ARGS="--directory install_dir --port 3030 \
-  --pouch-port 3040 --log debug --remote http://127.0.0.1:3031 \
+  --pouch-port 3040 --log error --remote http://127.0.0.1:3031 \
   --remote-skim http://127.0.0.1:3041/skimdb"
 
 if [[ $COVERAGE == 1 ]]; then
