1e210bfa5611bc82f1a53c7a45be28d2bb28fe60
Gabriel Csapo <gabecsapo@gmail.com>
2017-10-15 21:43:36 -0700

    2.2.2 - updates dependencies   - fixes issue #158 - fixes a bug where license is sometimes an object and not a string - uses serve-static instead of express.static (which is deprecated)

--- src/router.js
+++ src/router.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from './layout';
 import Main from './main';
 import Package from './package';
 
 export default (
-  <Router history={ browserHistory }>
-    <Route component={ Layout }>
-      <Route path="/_browse" component={ Main } />
-      <Route path="/_browse/package/:name" component={ Package } />
-    </Route>
-  </Router>
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path="/_browse" component={ Main } />
+        <Route exact path="/_browse/package/:name" component={ Package } />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );
